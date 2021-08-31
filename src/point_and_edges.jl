b838d5259d88d33c9c4ba5e913ef6c0a79a63290
Tabita Catalán <tabicm.nhg@gmail.com>
2021-08-30 20:21:01 -0400

    feat: structs Point and Edge, and some feats

---
+++ src/point_and_edges.jl
@@ -0,0 +1,32 @@
+using StructArrays, StaticArrays, LinearAlgebra 
+
+struct Point{T} <: FieldVector{2, T}
+    x::T
+    y::T
+end
+
+struct Edge{A <: AbstractArray{Point}} 
+    nodes::A
+end
+
+
+source(P::Edge) = P.nodes[1]
+target(P::Edge) = P.nodes[end]
+
+function Edge(source::Point, target::Point, subdivisions = 1)
+    N = subdivisions
+    nodos = StructArray(source + i/(N+1) * (target - source) for i in 0:(N+1))
+    Edge(nodos)
+end
+
+
+nodes(P::Edge) = @view P.nodes[:]
+subdivisions(P::Edge) = length(P.nodes) - 2
+
+inner_range(P::Edge) = 2:subdivisions(P)+1
+inner_nodes(P::Edge) = @view nodes(P)[inner_range(P)]
+
+midpoint(P::Edge) = (source(P) + target(P))/2
+asvector(P::Edge) = target(P) - source(P)
+len(P::Edge) = norm(asvector(P))
+
