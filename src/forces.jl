bbcc9d6a23ee0b4cd546cfd26c16e178cf994949
Tabita Catalán <tabicm.nhg@gmail.com>
2021-08-30 20:22:48 -0400

    feat: spring and electro forces

---
+++ src/forces.jl
@@ -0,0 +1,36 @@
+calculate_kp(P::Edge, K) = K/( len(P) * (subdivisions(P)+1)) 
+
+# Forces
+spring_force(p::Point, op::Point, kp) = kp * (op - p)
+electro_force(p::Point, op::Point, C) = p != op ? C * (op - p)/norm(op - p) : ForceFactor(0.0,0.0)
+
+
+ForceFactor = Point
+
+function force_on_node_i(i, edges, index)
+    forces_on_node_i_from_edge_j = (i, j) -> electro_force(nodes(edges[index])[i], nodes(edges[j])[i], compatibility(edges[index], edges[j]))
+    force = ForceFactor(0.0,0.0)
+    for j in 1:length(edges)
+        if index != j
+            force += forces_on_node_i_from_edge_j(i,j)
+        end
+    end
+    force
+end
+
+function calculate_electro_forces(edges, index)
+    StructArray(force_on_node_i(i,edges, index) for i in inner_range(edges[index]))
+end
+
+"""
+    calculate_spring_forces(P::Edge, K)
+Calculate spring forces acting on every inner node of an edge P.
+Returns a vector of Static Arrays.
+"""
+function calculate_spring_forces(P::Edge, K)
+    all_nodes = nodes(P)
+    kp = calculate_kp(P, K)
+    force_on_node_i = (i) -> spring_force(all_nodes[i], all_nodes[i-1], kp) + spring_force(all_nodes[i], all_nodes[i+1], kp)
+    StructArray(force_on_node_i(i) for i in inner_range(P))
+end
+
