1fee41975c9d89fa01554ac5298fc346dfb269ac
Tabita <tabicm.nhg@gmail.com>
2021-09-02 11:20:56 -0400

    test: read csv file

---
+++ test/read_csv_edges.jl
@@ -0,0 +1,24 @@
+using Test, ForceBundle 
+
+
+read_edges = ForceBundle.read_edges_csv("test\\testcase.csv")
+
+p0 = Point(-1.,0.)
+p1 = Point(0.,1.)
+q0 = Point(0.,-1.)
+q1 = Point(1.,0.)
+
+P = Edge(p0, p1)
+Q = Edge(q0, q1) 
+
+function are_equal(edge1, edge2)
+    for (node1, node2) in zip(ForceBundle.nodes(edge1), ForceBundle.nodes(edge2))
+        if node1 != node2
+            return false 
+        end 
+    end
+    return true 
+end 
+
+@test are_equal(P, read_edges[1])
+@test are_equal(Q, read_edges[2])
