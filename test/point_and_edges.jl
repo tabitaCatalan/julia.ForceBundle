827cca2e3f6f8f6803730b95359c48616223ea73
Tabita <tabicm.nhg@gmail.com>
2021-09-06 17:10:33 -0300

    test: ListOfNodes constructors

--- test/point_and_edges.jl
+++ test/point_and_edges.jl
@@ -6,6 +6,54 @@ using Test, ForceBundle
     @test p0.y == 2.
 end;
 
+@testset "list of nodes" begin
+    p0 = Point(0.,0.)
+    p1 = Point(0.,1.)
+    p2 = Point(0.,2.)
+    p3 = Point(0.,3.)
+    p4 = Point(0.,4.)
+    
+    L0 = ListOfNodes(p0,p4, subdivisions = 0)
+    L1 = ListOfNodes(p0,p4)
+    L3 = ListOfNodes(p0,p4, subdivisions = 3) 
+    
+    # L0 
+    @test L0[1] == p0
+    @test L0[end] == p4 
+    @test length(L0) == 2 
+
+    # L1
+    @test L1[1] == p0
+    @test L1[2] == p2
+    @test L1[end] == p4 
+    @test length(L1) == 3
+    
+    # L3 
+    @test L3[1] == p0
+    @test L3[2] == p1
+    @test L3[3] == p2
+    @test L3[4] == p3
+    @test L3[end] == p4
+    @test length(L3) == 5
+     
+end 
+
+
+@testset "list of nodes from list of points" begin
+    #=
+    test to create ListOfNodes from list of Points 
+    =# 
+    p1 = Point(0.,0.)
+    p2 = Point(1.,2.)
+    p3 = Point(1.,4.)
+    L = ListOfNodes([p1, p2, p3])
+    @test L[1] == p1
+    @test L[2] == p2
+    @test L[3] == p3 
+    @test length(L) == 3
+end  
+
+
 @testset "edge struct" begin
     p0 = Point(1.,2.)
     p1 = Point(3.,3.)
