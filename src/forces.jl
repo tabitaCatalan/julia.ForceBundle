a33c70163e2f2444c3fe5d211ffcf7df1c16e186
Tabita <tabicm.nhg@gmail.com>
2021-09-28 00:30:59 -0300

    docs: improved docs and comments in electric and spring forces functions

--- src/forces.jl
+++ src/forces.jl
@@ -1,18 +1,38 @@
 calculate_kp(P::Edge, K) = K/( len(P) * (subdivisions(P)+1)) 
 
 # Forces
+"""
+    spring_force(p::Point, op::Point, kp)
+wSpring force acting on `p` induced by `op`.
+# Arguments 
+- `kp`: spring coefficient. 
+"""
 spring_force(p::Point, op::Point, kp) = kp * (op - p)
+
+
+"""
+    electro_force(p::Point, op::Point, C)
+Electric force acting on `p` induced by `op`.
+# Arguments 
+- `C`: compatibility between `p` and `q`.
+"""
 electro_force(p::Point, op::Point, C) = p != op ? C * (op - p)/norm(op - p) : ForceFactor(0.0,0.0)
 
-
+# More descriptive name 
 ForceFactor = Point
 
-function force_on_node_i(i, edges, index)
-    forces_on_node_i_from_edge_j = (i, j) -> electro_force(nodes(edges[index])[i], nodes(edges[j])[i], compatibility(edges[index], edges[j]))
+"""
+# Arguments 
+- `edges`: a list of `Edge`s.
+- `index`: of edge which is being deformated.
+"""
+function eforce_on_node_i(i, edges, index) # TODO change variable to more descriptive ones 
+    # eforce on node i of index-th edge, induced by edge j (node i)
+    eforces_on_node_i_from_edge_j = (i, j) -> electro_force(nodes(edges[index])[i], nodes(edges[j])[i], compatibility(edges[index], edges[j]))
     force = ForceFactor(0.0,0.0)
-    for j in 1:length(edges)
-        if index != j
-            force += forces_on_node_i_from_edge_j(i,j)
+    for j in 1:length(edges) # recorrer todas las aristas 
+        if index != j # no induce fuerza sobre si mismo 
+            force += eforces_on_node_i_from_edge_j(i,j)
         end
     end
     force
