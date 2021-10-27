460b7b564cec66e24cc2e9cc429d78b047ad070f
Tabita <tabicm.nhg@gmail.com>
2021-10-26 23:45:47 -0300

    docs: add airlines example.jl in Literate.jl format

--- example/airlines.jl
+++ example/airlines.jl
@@ -1,23 +1,37 @@
+# # USA Airlines 
+
+# ## Import library 
 using ForceBundle 
 
-# # Read data from csv
-# Data can be read from a `.csv` file using `read_edges_csv` function. This uses `CSV.jl` library. 
+# ## Read data from csv
+# Data can be read from a `.csv` file using [`read_edges_csv`](@ref) function. This uses `CSV.jl` library. 
 # 
 # 
 # ```
 # -922.24444,-347.29444,-932.16944,-448.83333
 # -922.24444,-347.29444,-741.68611,-406.925
+# ...
 # ```
 
 edges = read_edges_csv("data\\airlines.csv"; subdivisions = 1);
-@enter forcebundle(edges, P = 2);
 
+# We select the first 100 edges 
+edges = edges[1:100];
+
+# ## Deform using [`forcebundle`](@ref) function 
+bundled_edges = forcebundle(edges, P_initial = 2);
+
+# ## Graficar 
 using Plots: plot, plot! 
+
+# A recipe is defined for `Edge`s, so we can use `plot(::Edge)` or `plot!(::Edge)`.
+# We plot straight edges
 
 a_plot = plot(title = "straight edges")
 plot!.(edges, color = :red)
 display(a_plot)
 
+# and bundled edges.
 b_plot = plot(title = "bundled edges")
 plot!.(bundled_edges, color = :red)
 display(b_plot)
