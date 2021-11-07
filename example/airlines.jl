5505d8800197494da0af1689cd63ecf8e95514ad
Tabita <tabicm.nhg@gmail.com>
2021-11-06 21:30:31 -0300

    docs: minor improvements in airlines example

--- example/airlines.jl
+++ example/airlines.jl
@@ -4,23 +4,25 @@
 using ForceBundle 
 
 # ## Read data from csv
-# Data can be read from a `.csv` file using [`read_edges_csv`](@ref) function. This uses `CSV.jl` library. 
-# 
+# `data/airlines.csv` content is as follows:
 # 
 # ```
 # -922.24444,-347.29444,-932.16944,-448.83333
 # -922.24444,-347.29444,-741.68611,-406.925
 # ...
 # ```
+# Data can be read from a `.csv` file using [`read_edges_csv`](@ref) function. This uses `CSV.jl` library. 
 DATA = joinpath(@__DIR__, "data", "airlines.csv")
+#DATA = "https://raw.githubusercontent.com/tabitaCatalan/julia.ForceBundle/master/data/airlines.csv"
 edges = read_edges_csv(DATA; subdivisions = 1);
 
 # We select the first 100 edges 
-edges = edges[1:100];
+edges = edges[1:200];
 
 # ## Bundle the edges 
 # We use the [`forcebundle`](@ref) function 
-bundled_edges = forcebundle(edges, P_initial = 2);
+
+bundled_edges = forcebundle(edges);                            );
 
 # ## Plot results  
 using Plots: plot, plot! 
@@ -28,11 +30,11 @@ using Plots: plot, plot!
 # A recipe is defined for `Edge`s, so we can use `plot(::Edge)` or `plot!(::Edge)`.
 # We plot straight edges
 
-a_plot = plot(title = "straight edges")
-plot!.(edges, color = :red)
+a_plot = plot(title = "straight edges");
+plot!.(edges, color = :red);
 display(a_plot)
 
 # and bundled edges.
-b_plot = plot(title = "bundled edges")
-plot!.(bundled_edges, color = :red)
+b_plot = plot(title = "bundled edges");
+plot!.(bundled_edges, color = :red);
 display(b_plot)
